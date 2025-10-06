f19dab57c55f7c3dce43563aa15e99acf69d3152
EsthaDevops <esthajames@yahoo.fr>
2025-10-06 11:11:40 -0700

    terraform commands automation

--- setup.sh
+++ setup.sh
@@ -11,9 +11,3 @@ unzip dev.zip
 cp -r /opt/web-consulting-dev/* /var/www/html
 
 
-
-/*
-At work we have couple engineer running terraform code locally after making changes to the tf file. the goal is to set up some type of automation to help run those commands. 
-since we are using github action already , do some research on how we can execute those terraform command thru a ci/cd pipeline. 
-also check if there are some terraform scaning tool that can be integrated in the pipeline
-*/
