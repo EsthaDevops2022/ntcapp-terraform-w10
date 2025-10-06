8cd1ea8e5cc43fbd6933c1f575f78a002476fb55
EsthaDevops <esthajames@yahoo.fr>
2025-10-06 12:50:03 -0700

    terraform commands automation

--- vpc.tf
+++ vpc.tf
@@ -97,6 +97,8 @@ resource "aws_route_table" "rtpublic" {
   }
 }
 
+/*
+
 # route table association public
 
 resource "aws_route_table_association" "rta1" {
@@ -117,3 +119,4 @@ resource "aws_route_table_association" "rta4" {
   subnet_id      = aws_subnet.private2.id
   route_table_id = aws_route_table.rtprivate.id
 }
+*/
